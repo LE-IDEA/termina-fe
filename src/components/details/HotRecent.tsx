daa49b3ed904566b6259ccd713c5929a7e32c331
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2025-03-02 04:02:56 +0100

    responsive add and withdraw page

--- src/components/details/HotRecent.tsx
+++ src/components/details/HotRecent.tsx
@@ -5,17 +5,17 @@ const instrumentSerif = Instrument_Serif({ weight: "400", subsets: ["latin"] });
 
 const HotRecent = () => {
     const recent = [
-        { imgURL: "/listing.png", name: "dev.fun annt", author: "devann", id: 1 },
-        { imgURL: "/listing.png", name: "dev.fun annt", author: "devann", id: 2 },
-        { imgURL: "/listing.png", name: "dev.fun annt", author: "devann", id: 3 },
-        { imgURL: "/listing.png", name: "dev.fun annt", author: "devann", id: 4 },
+        { imgURL: "/Trump.png", name: "dev.fun announcement", author: "devann", id: 1 },
+        { imgURL: "/Trump.png", name: "dev.fun announcement", author: "devann", id: 2 },
+        { imgURL: "/Trump.png", name: "dev.fun announcement", author: "devann", id: 3 },
+        { imgURL: "/Trump.png", name: "dev.fun announcement", author: "devann", id: 4 },
     ]
 
     return (
-        <div className="w-[364px] flex flex-col h-[213px] gap-[6px] ">
+        <div className="flex flex-col gap-[6px] md:min-w-[322px] xl:min-w-[480px]">
 
-            <div className=" flex flex-row w-[364px] h-[15px] justify-between pr-[6px] pl-[6px]">
-                <div className="flex flex-row items-center justify-between  w-[50px] h-[15px]">
+            <div className="hidden md:flex md:flex-row md:justify-between md:pr-[6px] md:pl-[6px]">
+                <div className="flex flex-row items-center justify-between">
                     <h1 className={`${instrumentSerif.className}`}>Recent</h1>
                     <Image src="/Clock.svg" alt='prev' width={12} height={12} />
                 </div>
@@ -24,30 +24,36 @@ const HotRecent = () => {
                 </h1>
             </div>
 
-            <div>
 
-                {/* 1st */}
-                {recent.slice(0, 3).map((item) => {
-                    return (
-                        <div key={item.id} className="w-[364px] h-[60px] p-[12px] gap-[10px] rounded-[18px] bg-[#ebebeb] flex flex-row">
-                            <Image src={item.imgURL} alt='Add' width={32} height={32} />
-                            <div className="w-[182px] h-[30px] flex flex-col">
-                                <h1 className={`${geologica.className} font-normal text-[20px] leading-[20px] tracking-[0%] text-black`}>{item.name}</h1>
+
+            {/* 1st */}
+            {recent.slice(0, 3).map((item) => {
+                return (
+
+                    <div key={item.id} className="p-[12px] rounded-[18px] bg-[#ebebeb] gap-[10px] flex flex-row  justify-between">
+                        <div className="gap-[10px] flex flex-row">
+                            <Image src={item.imgURL} alt='Add' width={32} height={32} className="w-[32px] h-[32px]" />
+                            <div className="flex flex-col max-w-[162px]">
+                                <h1 className={`${geologica.className} font-normal text-[20px] leading-[20px] tracking-[0%] text-black truncate`}>{item.name}</h1>
                                 <h1 className={`${geologica.className} font-normal text-[10px] leading-[10px] tracking-[0%] opacity-50`}>{item.author}</h1>
                             </div>
-                            <div className="flex flex-col w-[106px] h-[36px] justify-between">
-                                <div className="flex flex-row gap-[6px] items-center justify-end"> <Image src="/MCAP.svg" alt='prev' width={32} height={16} /> <h1 className={`${geologica.className} my-auto font-normal text-[20px] leading-[8px] tracking-[0%]`}>4.5m</h1>
-                                </div>
-                                <div className="w-[106px] h-[10px] gap-[12px] flex flex-row justify-between">
-                                    <h1 className={`${geologica.className} font-normal text-[10px] leading-[10px] tracking-[0%] opacity-50`}>0.00SOL</h1>
-                                    <h1 className={`${geologica.className} font-normal text-[10px] leading-[10px] tracking-[0%] text-[#FF0004]`}>-20%</h1>
-                                </div>
-                            </div>
-
                         </div>
-                    )
-                })}
-            </div>
+                        <div className="flex flex-col justify-between">
+                            <div className="flex flex-row gap-[6px] items-center justify-end">
+                                <Image src="/MCAP.svg" alt='prev' width={32} height={16} />
+                                <h1 className={`${geologica.className} my-auto font-normal text-[20px] leading-[8px] tracking-[0%]`}>4.5m</h1>
+                            </div>
+                            <div className="gap-[12px] flex flex-row justify-between">
+                                <h1 className={`${geologica.className} font-normal text-[10px] leading-[10px] tracking-[0%] opacity-50`}>0.0008SOL</h1>
+                                <h1 className={`${geologica.className} font-normal text-[10px] leading-[10px] tracking-[0%] text-[#FF0004]`}>-20%</h1>
+                            </div>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
