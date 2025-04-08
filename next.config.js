f74cd02ffe81c9196c09e8214f5626d39868c71c
dakewamama <82621391+dakewamama@users.noreply.github.com>
2025-04-08 12:37:35 +0000

    tried next with vite

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    output: 'export',
+    distDir: '.next',
+    reactStrictMode: true,
+    // This helps Next.js coexist with your Vite project
+    experimental: {
+      externalDir: true
+    }
+  }
