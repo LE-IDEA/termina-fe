000441cf692fb4ccdc31a96e8707dba3a6f412f0
dakewamama <82621391+dakewamama@users.noreply.github.com>
2025-04-19 13:12:47 +0000

    landing page 2

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight } from 'lucide-react'
 
 const CTASection = () => {
   return (
